03774a46f2a3abcb7f6fac151191075ac11c5d0c
Daniel Migales <daniel.migales.puertas@gmail.com>
2019-12-16 13:03:43 +0100

    v.1

--- src/ciclosformativos/Conexion.java
+++ src/ciclosformativos/Conexion.java
@@ -25,6 +25,7 @@ public class Conexion {
     private static ArrayList<String> tipoCampos = new ArrayList<>();
 
     public Conexion() {
+
         conection = null;
         try {
             Class.forName(driver);
@@ -38,15 +39,17 @@ public class Conexion {
     }
 
     public void eliminaTabla() throws SQLException {
+
         Statement st = null;
-        Scanner teclado = new Scanner(System.in);
+        Scanner tecladoStrings = new Scanner(System.in);
+
         try {
-            System.out.println("Introduce el nombre de la tabla a borrar: ");
-            String nombreTabla = teclado.nextLine();
+            System.out.println("Introduzca el nombre de la tabla que desea eliminar: ");
+            String nombreTabla = tecladoStrings.nextLine();
             st = conection.createStatement();
             String sql = "DROP TABLE IF EXISTS " + nombreTabla + "";
             st.executeUpdate(sql);
-            System.out.println("Tabla eliminada");
+            System.out.println("Tabla eliminada con exito.");
         } finally {
             if (st != null) {
                 st.close();
@@ -61,8 +64,10 @@ public class Conexion {
         String nombreCampo;
         int opcion, tipoCampo;
         String act;
+
         listaCampos = new ArrayList<>();
         tipoCampos = new ArrayList<>();
+
         Scanner teclado = new Scanner(System.in);
         Scanner tecladoStrings = new Scanner(System.in);
 
@@ -78,7 +83,7 @@ public class Conexion {
                     + "(id SERIAL PRIMARY KEY)";
             st.executeUpdate(sql);
 
-            System.out.println("Introduce el numero de campos de la tabla: ");
+            System.out.println("\nIntroduce el numero de campos de la tabla: ");
             opcion = teclado.nextInt();
 
             for (int i = 0; i < opcion; i++) {
@@ -137,28 +142,28 @@ public class Conexion {
 
     public void getCamposTabla() throws SQLException {
 
-        sc.nextLine();
+        Scanner tecladoStrings = new Scanner(System.in);
+
         listaCampos = new ArrayList<>();
         tipoCampos = new ArrayList<>();
-        getTabla(); // Llamar al metodo para escoger una tabla
-        System.out.println("Introduce el nombre de una tabla");
-        nombreTabla = sc.nextLine();
+        getTabla();
+
+        System.out.println("\nIntroduzca el nombre de la tabla: ");
+        nombreTabla = tecladoStrings.nextLine();
         Statement st = null;
         Statement st2 = null;
         String sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + nombreTabla + "'";
         String sql2 = "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + nombreTabla + "'";
 
         try {
-            // Consulta para recoger los nombres de los campos
 
-            st = conn.createStatement();
+            st = conection.createStatement();
             ResultSet rs = st.executeQuery(sql);
             while (rs.next()) {
                 listaCampos.add(rs.getString(1));
             }
 
-            // Consulta para recoger el tipo de datos 
-            st2 = conn.createStatement();
+            st2 = conection.createStatement();
             ResultSet rs2 = st.executeQuery(sql2);
             while (rs2.next()) {
                 tipoCampos.add(rs2.getString(1));
@@ -167,6 +172,157 @@ public class Conexion {
             if (st != null) {
                 st.close();
                 st2.close();
+            }
+        }
+    }
+
+    public void insertarDatos() throws SQLException {
+
+        getCamposTabla();
+        listaCampos.remove(0);
+        tipoCampos.remove(0);
+
+        Scanner tecladoStrings = new Scanner(System.in);
+
+        String sql = "INSERT INTO " + nombreTabla + "(";
+
+        for (int i = 0; i < listaCampos.size(); i++) {
+            sql = sql + listaCampos.get(i) + ",";
+        }
+
+        int lastIndexOf = sql.lastIndexOf(",");
+        sql = sql.substring(0, lastIndexOf);
+        sql = sql + ") VALUES (";
+
+        for (int i = 0; i < listaCampos.size(); i++) {
+            if (tipoCampos.get(i).equals("int")) {
+                System.out.println("Introduzca el dato del campo " + listaCampos.get(i) + " (numerico)");
+                String dato = tecladoStrings.nextLine();
+                sql = sql + "'" + dato + "',";
+            } else {
+                System.out.println("Introduzca el dato del campo " + listaCampos.get(i) + " (texto)");
+                String dato = tecladoStrings.nextLine();
+                sql = sql + "'" + dato + "',";
+            }
+        }
+
+        int indice = sql.lastIndexOf(",");
+        sql = sql.substring(0, indice);
+        sql = sql + ")";
+
+        Statement st = null;
+        try {
+            st = conection.createStatement();
+            st.executeUpdate(sql);
+            System.out.println("Datos añadidos.");
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+
+    public void editarDatos() throws SQLException {
+
+        getCamposTabla();
+        Statement st = null;
+        String sql;
+
+        Scanner tecladoStrings = new Scanner(System.in);
+        Scanner tecladoInts = new Scanner(System.in);
+
+        System.out.println("Listado de campos de la tabla: ");
+        System.out.println(listaCampos);
+
+        System.out.println("\nIntroduzca el ID del campo que quiere modificar: ");
+        int id = tecladoInts.nextInt();
+
+        System.out.println("Introduzca el nombre del campo a modificar: ");
+        String campo = tecladoStrings.nextLine();
+
+        System.out.println("Introduzca el tipo de dato del campo. Si el dato es texto escriba 1, si es numero escriba 2 : ");
+        int tipoDato = tecladoInts.nextInt();
+
+        if (tipoDato == 1) {
+            System.out.println("Introduzca el nuevo valor: ");
+            String nuevoValor = tecladoStrings.nextLine();
+            sql = "UPDATE " + nombreTabla + " SET " + campo + " = '" + nuevoValor + "' WHERE id = " + id + "";
+        } else {
+            System.out.println("Introduzca el nuevo valor: ");
+            int nuevoDato = tecladoInts.nextInt();
+            sql = "UPDATE " + nombreTabla + " SET " + campo + " = '" + nuevoDato + "' WHERE id = " + id + "";
+        }
+
+        try {
+            st = conection.createStatement();
+            st.executeUpdate(sql);
+            System.out.println("Registro actualizado con exito");
+
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+
+    public void borrarRegistro() throws SQLException {
+
+        getTabla();
+
+        Scanner tecladoStrings = new Scanner(System.in);
+        Scanner tecladoInts = new Scanner(System.in);
+
+        System.out.println("\nIntroduzca el nombre de la tabla: ");
+        nombreTabla = tecladoStrings.nextLine();
+        System.out.println("Introduzca el ID del registro que desea eliminar: ");
+        int id = tecladoInts.nextInt();
+
+        String sql = "DELETE FROM " + nombreTabla + " WHERE id = " + id + "";
+
+        Statement st = null;
+
+        try {
+            st = conection.createStatement();
+            st.executeUpdate(sql);
+            System.out.println("Registro eliminado con exito.");
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+
+    public void consultarCiclos() throws SQLException {
+
+        getCamposTabla();
+        String sql = "SELECT * FROM " + nombreTabla + "";
+
+        Statement st = null;
+
+        try {
+            st = conection.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            int contador = 0;
+            System.out.println(" ");
+
+            while (rs.next()) {
+                for (int i = 0; i < listaCampos.size(); i++) {
+                    String dato = rs.getString(listaCampos.get(i));
+                    System.out.println(listaCampos.get(i) + ": " + dato);
+                }
+
+                System.out.println(" ");
+
+                contador++;
+            }
+
+            if (contador == 0) {
+                System.out.println("No hay datos en esta tabla.");
+            }
+
+        } finally {
+            if (st != null) {
+                st.close();
             }
         }
     }
